3d5c23e1d73d9f8794cf095ecbd1791c8922c07b
CatherineCapellen <c.capellen@googlemail.com>
2014-10-14 22:32:42 +0200

    Created initial header files

--- blatt1/list.h
+++ blatt1/list.h
@@ -3,9 +3,21 @@
 
 struct node{
 	struct node *succ;
-	char *vorname;
-	char *nachname;
+	char *last_name;
+	char *first_name;
 };
+
+struct node * insert_sorted(struct node* list, struct node* new_entry);
+
+struct node * new_node();
+
+void print_list(const struct node*);
+
+void free_node(struct node*);
+
+void free_list(struct node*);
+
+
 
 
 
---
+++ blatt1/mystring.h
@@ -0,0 +1,17 @@
+#ifndef __h_mystring__
+#define __h_mystring__
+
+
+int compare_strings(const char* a, const char* b);
+
+char * find_char(const char * haystack, char needle);
+
+
+
+
+
+
+
+#endif
+
+
