bf4ea7bc8a8b15dcd6a128014be960c5305584ce
ben300694 <werbung_ben@live.de>
2014-10-14 23:04:51 +0200

    Added Comments

--- blatt1/list.h
+++ blatt1/list.h
@@ -1,20 +1,43 @@
 #ifndef __h_list__
 #define __h_list__
 
+/*
+ * Struct for a list that is capable of saving the first and last name of a person.
+ * succ is a pointer on the next node in the list
+ */
 struct node{
 	struct node *succ;
 	char *last_name;
 	char *first_name;
 };
 
+/*
+ * This function can insert a struct node, that is already filled with the names, at the right place in a list
+ * The list can be empty, then the new_node will be inserted at the first position
+ * If there are already elements in the list, the new_entry will be inserted such that the list is alphabetically ordered by last name.
+ * If there are two people with the same last_name the first_name is the next  for sorting.
+ * If there are two people with the same first_name and last_name the position doesn't matter. 
+ */
 struct node * insert_sorted(struct node* list, struct node* new_entry);
 
+/*
+ * Allocates memory for a new struct node and returns a pointer on the allocated memory.
+ */
 struct node * new_node();
 
+/*
+ * Prints the list.
+ */
 void print_list(const struct node*);
 
+/*
+ *Frees the memory allocated for the node, including all the memory allocated for the first_name, last_name.
+ */
 void free_node(struct node*);
 
+/*
+ * Frees a list, including all the memory allocated for the structs and the elements of the structs.
+ */
 void free_list(struct node*);
 
 
