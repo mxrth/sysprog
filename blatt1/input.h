f47cf7c42c972c7d71a5e9b1fde952bcc627a9cd
CatherineCapellen <c.capellen@googlemail.com>
2014-10-14 22:49:47 +0200

    Documented input and mystring

--- blatt1/input.h
+++ blatt1/input.h
@@ -11,12 +11,13 @@
  * and writes them into a newly allocated node struct.
  * If no space is present the line is interpreted as first name, last name is left empty (one NUL byte).
  * First and last name are allocated on the heap (just like the struct)
- * Returns 
+ * Returns a pointer to the newly allocated node, NULL on failure
  */
 struct node *read_name(FILE* in);
 
 /*
- * Tries to re
+ * Tries to read a filename from stdin an opens the file.
+ * Returns a file handle on success, NULL otherwise
  */
 FILE* get_input_file();
 
--- blatt1/mystring.h
+++ blatt1/mystring.h
@@ -1,10 +1,26 @@
 #ifndef __h_mystring__
 #define __h_mystring__
 
+enum Ordering {
+	Greater,
+	Equal,
+	Smaller
+}
 
-int compare_strings(const char* a, const char* b);
+/*
+ * Compares two strings lexically.
+ * If a is greater than b it returns Greater
+ * If a is equal to b it returns Equal
+ * If a is smaller than b it returns Smaller
+ * If a is a prefix of b and not equal to b, a is considered smaller
+ * The empty string is smaller than all other strings.
+ */
+Ordering compare_strings(const char* a, const char* b);
 
-char * find_char(const char * haystack, char needle);
+/*
+ * Returns a pointer to the first occurrence of `needle` in `haystack`, NULL if it is not present.
+ */
+char *find_char(const char * haystack, char needle);
 
 
 
