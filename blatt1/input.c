780f5c6997477ff9f54bbe251ef097450a33c50f
drak3 <drak3@live.de>
2014-10-17 00:44:10 +0200

    input.h is fully implemented, but not tested does not need find_char after all, sorry cat!

--- blatt1/input.c
+++ blatt1/input.c
@@ -1,13 +1,10 @@
 #include "input.h"
-#include "assert.h"
+#include "mystring.h"
+#include <assert.h>
 /*FIXME: Reimplement strlen*/
 #include <string.h>
 #include <stdlib.h>
-
-#define MAX_NAME_SIZE 100
-/*the additional chars are for linefeed*/
-#define BUFF_SIZE (MAX_NAME_SIZE * 2 + 2)
-
+#include <stdio.h>
 /*
  * Allocates space for num objects of type *dest and stores the resulting pointer to dest
  * Iff an error is encountered the statement after ALLOC_OR is executed
@@ -39,51 +36,54 @@ size_t string_copy(const char* src, char* dest, size_t dest_size) {
     return copied;
 }
 
+size_t string_len(const char *str) {
+    size_t len = 0;
+    for(;*str != '\0'; str++, len++);
+    return len;
+}
+
 struct node *read_name(FILE* fp) {
-    char buff[BUFF_SIZE];
-    char *space;
 
     size_t firstname_size, lastname_size;
     
     struct node* person;
-    char* first_name, *last_name;
-
-    if(fgets(buff, sizeof(buff), fp) == NULL) {
-	return NULL;
-    }
-
-    space = find_char(buff, ' ');
+    char first_name[101], last_name[101];
     
-    assert(space != NULL && "Expected input format: `Firstname<space>Lastname<linebreak>");
+    int result;
+
+    result = fscanf(fp, "%100s %100s", first_name, last_name);
     
-    /*Off by ones ahead!*/
-    /*
-     * 0123456789
-     * firs last0
-     */
-    *space = '\0';
-     
-     /*
-      * 0123456789
-      * firs0last0
-      * Also count the terminating NUL byte
-      */
-    firstname_size = strlen(buff)+1;
-    lastname_size = strlen(space+1)+1;
+    if(result < 2 || result == EOF) return NULL;
+
+    firstname_size = string_len(first_name);
+    lastname_size = string_len(last_name);
     
     /* allocate some stuff */
     person = new_node();
     if(!person) return NULL;
     
-    ALLOC_OR(first_name, firstname_size) return NULL;
-    ALLOC_OR(last_name, lastname_size) return NULL;
+    ALLOC_OR(person->first_name, firstname_size) return NULL;
+    ALLOC_OR(person->last_name, lastname_size) return NULL;
 
-    string_copy(buff, first_name, firstname_size);
-    string_copy(space+1, last_name, lastname_size);
+    string_copy(last_name, person->first_name, firstname_size);
+    string_copy(first_name, person->last_name, lastname_size);
 
-    person->first_name = first_name;
-    person->last_name = last_name;
 
     return person;
     
 }
+
+FILE *get_input_file(void) {
+    char path[255];
+    printf("Please input file name");
+    scanf("%255s", path);
+    return fopen(path, "r");
+}
+
+
+
+
+
+
+
+
