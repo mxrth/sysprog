63fa622f29871c10128840723def43222d2edb7c
drak3 <drak3@live.de>
2014-10-17 17:26:41 +0200

    Implemented main.c

--- blatt1/main.c
+++ blatt1/main.c
@@ -1,5 +1,23 @@
+#include "input.h"
+#include "list.h"
 
+#include <stdlib.h>
+
+#define ERROR(s) {fprintf(stderr, "%s", (s)); exit(EXIT_FAILURE);}
 
 int main(void) {
-    return 0;
+    FILE* in_file;
+
+    struct node* person, *list = NULL;
+
+    in_file = get_input_file(); 
+    if(!in_file) ERROR("Could not open input file")
+
+    while((person = read_name(in_file)) != NULL) {
+	list = insert_sorted(list, person);
+    }
+
+    print_list(list);
+
+    return EXIT_SUCCESS;
 }
