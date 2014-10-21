2802d67b82c5c37eb7f3dd5eabee580e23759d3c
CatherineCapellen <c.capellen@googlemail.com>
2014-10-21 10:37:26 +0200

    frei() ist gecoded. Auch noch nicht getestet.

--- blatt2/aufgabe4/brett.c
+++ blatt2/aufgabe4/brett.c
@@ -22,11 +22,35 @@ int** allocate_feld(int n){
 	return feld;
 }
 
+/*Führt Initialisierung des Schachbrettes durch.
+Ruft allocate_feld auf. Markiert Startposition im Feld. Gibt 0 zurück, wenn Initialisierung nicht durchgeführt werden konnte*/
 int init_brett(struct t_brett *b, int n, int x, int y){
-	(&b).felder = allocate_feld(n);
-	if(&b != NULL) {
-	(&b).felder[x][y] = 1;
+	(*b).felder = allocate_feld(n);
+	if(*b != NULL) {
+	(*b).felder[x][y] = 1;
 	return 1;
 	}
 	return 0;
 }
+
+
+/* Bewegt Springer um x Felder in die Horizontalen und um y Felder in der Vertikalen.
+Berechnet die neue Position und markiert das entsprechende Feld als besucht*/
+int neuer_sprung(struct t_brett *b, int x, int y);
+
+/*Macht den Sprung (x,y) rückgängig und entfernt die Markierung */
+int entferne_sprung(struct t_brett *b, int x, int y);
+
+/*Liefert Wert ob das Feld noch besucht werden kann*/
+int frei(struct t_brett *b, int x, int y) {
+	if((*b).felder[x][y] >0) {
+		return 0;
+	}
+		return 1;
+}
+
+/*Liefert Anzahl an Feldern, die schon vom Springer besucht worden sind*/
+int besuchte_felder(struct t_brett *b);
+
+/*Gibt reservierten Speicher wieder frei*/
+void loesche_brett(struct t_brett *b);
