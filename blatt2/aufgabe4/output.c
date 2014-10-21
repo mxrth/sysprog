ff2798f673de8b675efb3926810ccbb6e60a5dd2
ben300694 <werbung_ben@live.de>
2014-10-21 22:39:00 +0200

    Added output.c

---
+++ blatt2/aufgabe4/output.c
@@ -0,0 +1,51 @@
+/*Benoetigt fuer:
+ *
+ */
+#include "output.h"
+
+/*Benoetigt fuer:
+ *struct t_brett
+ *
+ */
+#include "brett.h"
+
+/*Benoetigt fuer:
+ *printf
+ */
+#include <stdio.h>
+
+/*gibt aktuelles Schachbrett aus */
+void print_brett(struct t_brett *b){
+	int i;
+	for(i=0;i<b->dimension;i++){
+		print_seperating_line(b->dimension);
+		print_array_line(b, i);
+	}
+	print_seperating_line(b->dimension);
+	return;
+}
+
+/*Assumption: n is an integer >1
+ *
+ *Result: prints n instances of +---+ in the command line
+ */
+void print_seperating_line(int length){
+	int i;
+	for(i=0;i<length;i++){
+		printf("+---");
+	}
+	printf("+");
+	return;
+}
+
+/*Assumption: b is an initialized struct t_brett, n is an integer with 1<= n <=b->dimension
+ *
+ *Result: prints the nth line of the chess board, colums seperated by "+"
+ */
+void print_array_line(struct t_brett* b, int n){
+	int i;
+	for(i=0;i<b->dimension;i++){
+		printf("+%3i",b->felder[n][i]);
+	}
+	printf("+");
+}
