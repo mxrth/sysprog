8cc47e49df34a0030a005d3cd119b0eb25b10a0d
drak3 <drak3@live.de>
2014-10-26 20:18:30 +0100

    Foo

--- blatt3/aufgabe8/matrixgen_main.c
+++ blatt3/aufgabe8/matrixgen_main.c
@@ -1,5 +1,19 @@
 #include "matrix.h"
+#include "util.h"
 
-int main(void) {
-
+int main(int argc, char *argv[]) {
+    size_t rows=0, columns=0;
+    Matrix *m;
+    if(argc != 3) {
+	ERROR("Please specify number of rows and columns");
+    }
+    sscanf(argv[1], "%zu", &rows);
+    sscanf(argv[2], "%zu", &columns);
+    if(rows <= 0 || columns <= 0)
+	ERROR("Invalid number of rows or columns");
+    
+    m = matrix_generate_random(rows, columns);
+    if(!m) ERROR("Could not generate matrix");
+    matrix_print(m);
+    return 0;
 }
--- blatt3/aufgabe8/util.h
+++ blatt3/aufgabe8/util.h
@@ -1,6 +1,6 @@
 #ifndef _UTIL_H__
 #define _UTIL_H__
-
+#include <stdlib.h>
 #define ERROR(s) {printf("ERROR:\n %s",s);exit(1);}
 
 
