28bb439a08ce03f1224735cec36d69d22c6d45fa
drak3 <drak3@live.de>
2014-10-26 20:00:28 +0100

    Started implementing matrixgen_main

---
+++ blatt3/aufgabe8/matrix_alloc.c
@@ -0,0 +1,19 @@
+#include "matrix.h"
+#include <stdlib.h>
+
+Matrix* matrix_allocate(size_t rows, size_t columns) {
+    Matrix *mat;
+    size_t i;
+
+    if(!( mat = malloc(sizeof(*mat) )) ) return NULL;
+    mat->rows = rows;
+    mat->columns = columns;
+    mat->data = malloc( sizeof(*(mat->data)) * rows);
+    if(!mat->data) return NULL;
+
+    for(i = 0; i < rows; i++) {
+	if(! (mat->data[i] = malloc(sizeof(*(mat->data[i]))*columns)) )
+	    return NULL;
+    }
+    return mat;
+}
