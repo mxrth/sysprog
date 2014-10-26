d5799e1b7e7c68432f9384336c9f0a1ae9c27d99
ben300694 <werbung_ben@live.de>
2014-10-26 20:04:38 +0100

    Added matrix_read

--- blatt3/aufgabe8/main.c
+++ blatt3/aufgabe8/main.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "matrix.h"
 
+#define ERROR(s) {printf("ERROR:\n %s",s);exit(1);}
 
+/*The program expects one argument:
+ *The user has to enter the name of a .txt file in which a matrix is stored.
+ *The first line contains the number of rows and columns seperated by a space ' '
+ *After this the lines of the matrix follow. Each value is seperated from the next by a space ' '
+ *After each row of matrix data there has to be a new line
+ *
+ *Example:
+ *===Begin of file===
+ *5 5
+ *7 8 6 4 5
+ *5 8 4 2 5
+ *7 -8 7 -45 5
+ *-7 8 2 -23 5
+ *7 -8 9 3 5
+ *===End of file===
+ */
 int main(int argc, char* argv[]){
+	FILE* text;
+	Matrix *A;	
+
+	if(argc < 2){
+		ERROR("Not the right amount of arguments")
+	}
 	
+	text = fopen(argv[1],"r");
 	
+	if(text == NULL){
+		ERROR("Could not locate the file")	
+	}
+
+	A=matrix_read(text);
+	if(A==NULL){
+		ERROR("Fehler beim Einlesen der Matrix")
+	}
+	fclose(text);
+
+	matrix_print(A);
 
 	return 0;
 }
---
+++ blatt3/aufgabe8/util.h
@@ -0,0 +1,7 @@
+#ifndef _UTIL_H__
+#define _UTIL_H__
+
+#define ERROR(s) {printf("ERROR:\n %s",s);exit(1);}
+
+
+#endif
