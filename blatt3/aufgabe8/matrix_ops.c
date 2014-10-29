6ae96f340f116f608c837e91e5f776fbe7518eb1
CatherineCapellen <c.capellen@googlemail.com>
2014-10-29 15:25:45 +0100

    Added the add_matrix function, but haven't tested it yet.

--- blatt3/aufgabe8/matrix_ops.c
+++ blatt3/aufgabe8/matrix_ops.c
@@ -9,3 +9,19 @@ void matrix_fill(Matrix* A,long a){
 	}
 	return;
 }
+
+
+int matrix_add(Matrix *A,Matrix *B,Matrix *Result){
+	/*check dimensions */
+	if(A->rows != B->rows || A->rows != Result->rows ||A->columns != B->columns || A->columns != Result->columns) {
+	return 1;
+	}
+	/*calculate */
+	size_t i,j;
+	for(i=0;i<A->rows;i++){
+		for(j=0;j<A->columns;j++){
+			Result->data[i][j] = A->data[i][j] + B->data[i][j];
+		}
+	}
+	return 0;
+}
